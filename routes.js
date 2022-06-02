7eadcccd8300c9979366a3a6fb760378aabb3e26
Javier <63475312+jotarexx@users.noreply.github.com>
2022-06-02 16:05:34 -0300

    Add files via upload

---
+++ routes.js
@@ -0,0 +1,17 @@
+
+// declaramos un array donde declaramos nuestras 3 rutas
+
+const routes = [
+  {
+    path:'/', // valor que consultaremos (home)
+    template: '<h1>Hola</h1>'
+  },
+  {
+    path:'/contacto', 
+    template: '<h1>Contacto</h1>'
+  },
+  {
+    path:'about',
+    template: '<h1>About Me</h1>'
+  },
+];
