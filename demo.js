8b93c5b425f2cc0ec6e401a492256b249a852946
Javier <63475312+jotarexx@users.noreply.github.com>
2022-06-03 11:10:49 -0300

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,6 @@
+
+window.history.pushState({data: 'Movimiento'}, 'Title', 'path');
+
+// movernos en el titulo/parte del navegador en JS, ej:
+// window.history.pushState({data: 'Movimiento'}, 'Title', '/');
+// nos moverá al titulo del nav en la parte HOME (/)
