08eac552b03cc01e8efa78bb24a47002564884c7
Javier <63475312+jotarexx@users.noreply.github.com>
2022-06-08 20:50:27 -0300

    Update demo.js

--- demo.js
+++ demo.js
@@ -3,4 +3,4 @@ window.history.pushState({data: 'Movimiento'}, 'Title', 'path');
 
 // movernos en el titulo/parte del navegador en JS, ej:
 // window.history.pushState({data: 'Movimiento'}, 'Title', '/');
-// nos moverá al titulo del nav en la parte HOME (/)
+
